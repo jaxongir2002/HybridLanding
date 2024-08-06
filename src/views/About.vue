7cf6fd7fc8d346678ab06edf4ab5df78222a1ad6
jaxongirxon <jaxongirxon.rustamov12@gmail.com>
2024-08-06 20:13:01 +0500

    bug fix mobile version

--- src/views/About.vue
+++ src/views/About.vue
@@ -1,30 +1,29 @@
 <script setup>
-import { defineAsyncComponent } from 'vue';
-
-const Navigation = defineAsyncComponent(() => import('@/components/Navigation.vue'));
-const Cover = defineAsyncComponent(() => import('@/components/Cover.vue'));
-const Cards = defineAsyncComponent(() => import('@/components/Cards.vue'));
-const Mission = defineAsyncComponent(() => import('@/components/Mission.vue'));
-const Magic = defineAsyncComponent(() => import('@/components/Magic.vue'));
-const OurWorks = defineAsyncComponent(() => import('@/components/OurWorks.vue'));
-const Partners = defineAsyncComponent(() => import('@/components/Partners.vue'));
-const Register = defineAsyncComponent(() => import('@/components/Register.vue'));
-const LastSection = defineAsyncComponent(() => import('@/components/LastSection.vue'));
-const Loading = defineAsyncComponent(() => import('@/components/Loading.vue'));
+import Navigation from "@/components/Navigation.vue";
+import Cover from "@/components/Cover.vue";
+import Cards from "@/components/Cards.vue";
+import Mission from "@/components/Mission.vue";
+import Magic from "@/components/Magic.vue";
+import OurWorks from "@/components/OurWorks.vue";
+import Partners from "@/components/Partners.vue";
+import Register from "@/components/Register.vue";
+import LastSection from "@/components/LastSection.vue";
+import Loading from "@/components/Loading.vue";
 </script>
 
 <template>
   <div class="container-hybrid">
-    <Navigation />
-    <Cover />
-    <Cards />
-    <Mission />
-    <Magic />
-    <OurWorks />
-    <Partners />
-    <Register />
-    <LastSection />
+    <Navigation/>
+    <Cover/>
+    <Cards/>
+    <Mission/>
+    <Magic/>
+    <OurWorks/>
+    <Partners/>
+    <Register/>
+    <LastSection/>
   </div>
+
 </template>
 
 <style scoped lang="scss">
