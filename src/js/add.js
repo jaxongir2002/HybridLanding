d223ac6b88178e54b92a68196f0468d38e1759f0
user <jaxongirxon.rustamov12@gmail.com>
2024-10-12 20:40:10 +0500

    bug fix about app pages

--- src/js/add.js
+++ src/js/add.js
@@ -1,9 +1,3 @@
-// If Fluid is a named export
-// import { Fluid } from './fluid.min.js';
-
-// Or if it's a default export
-// import Fluid from './fluid.min.js';
-
 document.addEventListener("DOMContentLoaded", function() {
     const canvas = document.getElementById("renderSurface");
     let myFluid = new Fluid(canvas); // Assuming Fluid is a class
